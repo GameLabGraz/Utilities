7f0864532ee03f2dffb7975d261806c068fc7056
Michael Stefan Holly <michael.holly@tugraz.at>
2020-03-21 13:11:23 +0100

    Initial commit

---
+++ PushPackages.bat
@@ -0,0 +1,7 @@
+git subtree push --prefix Assets/Plugins/LanguageManager origin package/languagemanager
+git subtree push --prefix Assets/Plugins/QuestManager origin package/questmanager
+git subtree push --prefix Assets/Plugins/SerializeProperty origin package/serializeproperty
+
+@echo off
+echo Press enter to exit
+set /p input=
