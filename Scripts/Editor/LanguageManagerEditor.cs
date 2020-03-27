88a13c64fdca8f536920e25fef39f8d4dc78ad59
Michael Stefan Holly <michael.holly@tugraz.at>
2020-03-27 14:36:05 +0100

    GEAR-4 Create Unity Unit Tests for LanguageManager

---
+++ Scripts/Editor/LanguageManagerEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace GEAR.Localization.Editor
+{
+    [CustomEditor(typeof(LanguageManager))]
+    public class LanguageManagerEditor : UnityEditor.Editor
+    {
+        private const string TexturePath = "images/logo";
+        private Texture2D _logoTexture;
+        private void Awake()
+        {
+            _logoTexture = Resources.Load<Texture2D>(TexturePath);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawLogoTexture();
+            DrawDefaultInspector();
+        }
+
+        private void DrawLogoTexture()
+        {
+            if (_logoTexture)
+            {
+                GUILayout.Box(_logoTexture, GUILayout.Width(90f), GUILayout.Height(60f), GUILayout.ExpandWidth(true));
+            }
+        }
+    }
+}
---
+++ Tests/Runtime/LocalizedTMPTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using TMPro;
+using GEAR.Localization;
+
+public class LocalizedTMPTest
+{
+    private LanguageManager _languageManager;
+    private LocalizedTMP _localizedTMP;
+
+    [UnitySetUp]
+    public IEnumerator Setup()
+    {
+        yield return new EnterPlayMode();
+
+        _languageManager = new GameObject("LanguageManager").AddComponent<LanguageManager>();
+        _languageManager.LoadMlgFile(Resources.Load<TextAsset>("ValidMLG"));
+
+        var localizedTextObj = new GameObject("LocalizedText");
+        localizedTextObj.AddComponent<TextMeshPro>();
+
+        _localizedTMP= localizedTextObj.AddComponent<LocalizedTMP>();
+        _localizedTMP.Key = "GER";
+    }
+
+    [UnityTearDown]
+    public IEnumerator Teardown()
+    {
+        Object.Destroy(_languageManager);
+        Object.Destroy(_localizedTMP);
+
+        yield return new ExitPlayMode();
+    }
+
+    [UnityTest]
+    [TestCase(SystemLanguage.English, "German", ExpectedResult = null)]
+    [TestCase(SystemLanguage.German, "Deutsch", ExpectedResult = null)]
+    public IEnumerator TestLocalizedText(SystemLanguage language, string expected)
+    {
+        var tmpText = _localizedTMP.GetComponent<TMP_Text>();
+
+        _languageManager.CurrentLanguage = language;
+
+        yield return new WaitForFixedUpdate();
+
+        Assert.AreEqual(expected, tmpText.text);
+    }
+
+}
