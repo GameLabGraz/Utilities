b12c33d8f8cec90ec5919b5c295850852aa7cbb1
Michael Stefan Holly <michael.holly@tugraz.at>
2020-05-05 15:55:26 +0200

    GEAR LimeSurvey Uploader

---
+++ Scripts/Runtime/LimeSurveyTypes.cs
@@ -0,0 +1,46 @@
+﻿using GEAR.Utilities.Attribute;
+
+namespace GEAR.LimeSurvey
+{
+    public enum LimeSurveyField
+    {
+        [StringValue("user")] User,
+        [StringValue("password")] Password,
+        [StringValue("loginlang")] Language,
+        [StringValue("action")] Action,
+        [StringValue("subaction")] SubAction,
+        [StringValue("sid")] Sid,
+        [StringValue("the_file")] File,
+        [StringValue("noid")] NoId,
+        [StringValue("insert")] Insert,
+        [StringValue("finalized")] Finalized,
+        [StringValue("vvcharset")] Charset
+    }
+
+    public enum LimeSurveyLanguage
+    {
+        [StringValue("default")] Default,
+        [StringValue("en")] English,
+        [StringValue("de")] German,
+    }
+
+    public enum LimeSurveyAction
+    {
+        [StringValue("login")] Login,
+        [StringValue("upload")] Upload,
+        [StringValue("vvimport")] Import
+    }
+
+    public enum LimeSurveyInsertType
+    {
+        [StringValue("ignore")] Ignore,
+        [StringValue("renumber")] Renumber,
+        [StringValue("replace")] Replace
+    }
+
+    public enum LimeSurveyCharset
+    {
+        [StringValue("utf8")] Utf8,
+        [StringValue("ascii")] Ascii
+    }
+}
