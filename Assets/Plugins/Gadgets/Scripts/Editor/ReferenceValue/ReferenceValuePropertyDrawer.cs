ba10058fbfec95d173b7a921098b982e1eaa0887
Michael Stefan Holly <michael.holly@tugraz.at>
2020-05-26 10:34:01 +0200

    Utilities-20 [Gadgets] ReferenceValue

---
+++ Assets/Plugins/Gadgets/Scripts/Editor/ReferenceValue/ReferenceValuePropertyDrawer.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace GEAR.Gadgets.ReferenceValue.Editor
+{
+    public class ReferenceValuePropertyDrawer<T> : PropertyDrawer
+    {
+        private const int LineHeight = 16;
+        private const int IndentWidth = 190;
+        private static int MarginBetweenFields => 2 * (int)EditorGUIUtility.standardVerticalSpacing;
+
+        private const BindingFlags BindingFlags = System.Reflection.BindingFlags.Instance | 
+                                                  System.Reflection.BindingFlags.Public | 
+                                                  System.Reflection.BindingFlags.NonPublic;
+
+        private Rect _position;
+        private int _totalPropertyHeight;
+        
+        private SerializedProperty _property;
+        private FieldInfo _propertyFieldInfo;
+        
+        private Object Target => _property.serializedObject.targetObject;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var propertyName = property.propertyPath.Split('.');
+            _property = property.serializedObject.FindProperty(propertyName[0]);
+
+            if (_propertyFieldInfo == null)
+                _propertyFieldInfo = Target.GetType().GetField(_property.propertyPath, BindingFlags);
+
+            if (_propertyFieldInfo == null) return;
+
+            EditorGUI.BeginChangeCheck();
+
+            var referenceValue = GetReferenceValue(property);
+            if (referenceValue == null) return;
+
+            _position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            SetPosition();
+            IncrementPosition();
+
+            referenceValue.objectInfo.Object = EditorGUI.ObjectField(_position, "Object",
+                referenceValue.objectInfo.Object, typeof(Object), true);
+
+            var components = new List<Component>();
+            var fields = new List<FieldInfo>();
+            var fieldNames = new List<string>();
+
+            if (referenceValue.objectInfo.Object != null)
+            {
+                if (referenceValue.objectInfo.Object is GameObject gameObject)
+                {
+                    foreach (var component in gameObject.GetComponents<Component>())
+                    {
+                        fields.AddRange(component.GetType()
+                            .GetFields(BindingFlags)
+                            .Where(field => typeof(T).IsAssignableFrom(field.FieldType)));
+
+                        foreach (var field in fields)
+                        {
+                            fieldNames.Add($"{component}{component.GetInstanceID()}/{field.Name}");
+                            components.Add(component);
+                        }
+                    }
+                }
+                else
+                {
+                    fields.AddRange(referenceValue.objectInfo.Object.GetType()
+                        .GetFields(BindingFlags)
+                        .Where(field => typeof(T).IsAssignableFrom(field.FieldType)));
+
+                    fieldNames.AddRange(fields
+                        .Select(field => field.Name));
+                }
+            }
+
+            IncrementPosition();
+            
+            referenceValue.index = EditorGUI.Popup(_position, "Value", referenceValue.index, fieldNames.ToArray());
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                referenceValue.objectInfo.FieldName = fieldNames[referenceValue.index];
+                referenceValue.objectInfo.ComponentName = components.Count > 0 ? 
+                    components[referenceValue.index].GetType().Name : null;
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return _totalPropertyHeight;
+        }
+
+        private void SetPosition()
+        {
+            _position.height = LineHeight;
+            _totalPropertyHeight = LineHeight;
+            _position.x -= IndentWidth;
+            _position.width += IndentWidth;
+        }
+
+        private void IncrementPosition()
+        {
+            _position.y += LineHeight + MarginBetweenFields;
+            _totalPropertyHeight += LineHeight + MarginBetweenFields;
+        }
+
+        private ReferenceValue<T> GetReferenceValue(SerializedProperty property)
+        {
+            ReferenceValue<T> referenceValue;
+
+            var value = _propertyFieldInfo.GetValue(Target);
+            switch (value)
+            {
+                case null:
+                case IList referenceValues when
+                    referenceValues.Count == 0 || GetArrayIndex(property.propertyPath) >= referenceValues.Count:
+                    return null;
+                case IList referenceValues:
+                {
+                    var index = GetArrayIndex(property.propertyPath);
+                    referenceValue = (ReferenceValue<T>)referenceValues[index];
+                    break;
+                }
+                default:
+                    referenceValue = (ReferenceValue<T>)value;
+                    break;
+            }
+
+            return referenceValue;
+        }
+
+        private static int GetArrayIndex(string propertyPath)
+        {
+            return int.TryParse(
+                Regex.Match(propertyPath, @".+\[(\d+)]").Groups[1].Value, out var index) 
+                ? index : -1;
+        }
+    }
+
+    [CustomPropertyDrawer(typeof(BooleanReferenceValue))]
+    public class BooleanReferenceValuePropertyDrawer : ReferenceValuePropertyDrawer<bool> { }
+
+    [CustomPropertyDrawer(typeof(StringReferenceValue))]
+    public class StringReferenceValuePropertyDrawer : ReferenceValuePropertyDrawer<string> { }
+
+    [CustomPropertyDrawer(typeof(IntegerReferenceValue))]
+    public class IntegerReferenceValuePropertyDrawer : ReferenceValuePropertyDrawer<int> { }
+
+    [CustomPropertyDrawer(typeof(FloatReferenceValue))]
+    public class FloatReferenceValuePropertyDrawer : ReferenceValuePropertyDrawer<float> { }
+
+    [CustomPropertyDrawer(typeof(Vector2ReferenceValue))]
+    public class Vector2ReferenceValuePropertyDrawer : ReferenceValuePropertyDrawer<Vector2> { }
+
+    [CustomPropertyDrawer(typeof(Vector3ReferenceValue))]
+    public class Vector3ReferenceValuePropertyDrawer : ReferenceValuePropertyDrawer<Vector3> { }
+}
---
+++ Assets/Scenes/Gadgets/Scripts/ReferenceValueExample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using GEAR.Gadgets.ReferenceValue;
+using UnityEngine;
+
+public class ReferenceValueExample : MonoBehaviour
+{
+    [SerializeField] private IntegerReferenceValue intReferenceValue;
+    [SerializeField] private List<FloatReferenceValue> floatReferenceValues;
+
+    private void Start()
+    {
+        intReferenceValue.Value = 1;
+
+        foreach (var floatReferenceValue in floatReferenceValues)
+        {
+            floatReferenceValue.Value = 2.3f;
+        }
+    }
+}
