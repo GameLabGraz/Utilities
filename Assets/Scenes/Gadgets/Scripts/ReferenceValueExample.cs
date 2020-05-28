574fc1491d43da40c65b1a1e8232bfc65efb61f2
Sandra <brettschuh@student.tugraz.at>
2020-05-28 13:56:38 +0200

    reference value fix

--- Assets/Scenes/Gadgets/Scripts/ReferenceValueExample.cs
+++ Assets/Scenes/Gadgets/Scripts/ReferenceValueExample.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
 using GEAR.Gadgets.ReferenceValue;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ReferenceValueExample : MonoBehaviour
 {
     [SerializeField] private IntegerReferenceValue intReferenceValue;
     [SerializeField] private List<FloatReferenceValue> floatReferenceValues;
+    
+    public UnityEvent onExample = new UnityEvent();
 
     private void Start()
     {
