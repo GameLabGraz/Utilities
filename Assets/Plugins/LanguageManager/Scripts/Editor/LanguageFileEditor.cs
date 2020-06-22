09aab75f3cbd1d2301e06b7d632efdc952fb26d0
Sandra <brettschuh@student.tugraz.at>
2020-06-22 17:33:28 +0200

    language editor window

---
+++ Assets/Plugins/LanguageManager/Scripts/Editor/LanguageFileEditor.cs
@@ -0,0 +1,264 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Schema;
+using UnityEditor;
+using UnityEngine;
+
+namespace GEAR.Localization.Editor
+{
+    public class LanguageFileEditor : EditorWindow
+    {
+        [MenuItem("Window/Language Editor")]
+        public static void ShowWindow()
+        {
+            GetWindow<LanguageFileEditor>("Language Editor");
+        }
+
+        Vector2 scrollPositionContent = Vector2.zero;
+        Vector2 scrollPositionLanguage = Vector2.zero;
+        private Translation addingTranslation = null;
+        private string newLanguage = "";
+
+
+        private bool languageFoldout = true;
+        private string currentPath = "";
+        private readonly HashSet<SystemLanguage> removedLanguages = new HashSet<SystemLanguage>();
+        private readonly HashSet<SystemLanguage> addedLanguages = new HashSet<SystemLanguage>();
+        private HashSet<SystemLanguage> supportedLanguages = new HashSet<SystemLanguage>();
+        private readonly Dictionary<string, Translation> translations = new Dictionary<string, Translation>();
+        private bool reloaded = true;
+        
+        // Start is called before the first frame update
+        void OnGUI()
+        {
+            var style = EditorStyles.miniButton;
+            style.fixedWidth = 55;
+            style.alignment = TextAnchor.MiddleCenter;
+            
+            GUILayout.Label("Language Editor", EditorStyles.largeLabel);
+            GUILayout.Label("You can edit your MLG files within this editor. If you make any changes, " +
+                            "please do not forget to save them before selecting another file.", EditorStyles.helpBox);
+            
+            var path = "";
+            var obj = Selection.activeObject;
+            path = obj == null ? "Assets" : AssetDatabase.GetAssetPath(obj.GetInstanceID());
+            if (path.Length <= 0)
+            {
+                EditorGUILayout.TextField("File", "Invalid Selection");
+                return;
+            }
+            
+            EditorGUILayout.TextField("File", path);
+            var textAsset = Selection.activeObject as TextAsset;
+            if (Directory.Exists(path) || !textAsset) {
+                GUILayout.Label("Please select a MLG file to edit the content.", EditorStyles.helpBox);
+                return;
+            }
+
+            if (reloaded)
+            {
+                addingTranslation = null;
+                currentPath = "";
+                translations.Clear();
+                supportedLanguages.Clear();
+                addedLanguages.Clear();
+                removedLanguages.Clear();
+                textAsset = Selection.activeObject as TextAsset;
+        
+                var xmlSchemaSet = new XmlSchemaSet();
+                xmlSchemaSet.Add("", XmlReader.Create(new MemoryStream(Resources.Load<TextAsset>("mlgSchema").bytes)));
+                if (!LanguageManager.LoadMlgFile(textAsset, xmlSchemaSet, translations, out var lastError,
+                    out supportedLanguages, false))
+                {
+                    GUILayout.Label("Error: " + lastError, EditorStyles.helpBox);
+                    return;
+                }
+
+                reloaded = false;
+            }
+
+            foreach (var removedLang in removedLanguages)
+            {
+                supportedLanguages.Remove(removedLang);
+            }
+            foreach (var addedLang in addedLanguages)
+            {
+                supportedLanguages.Add(addedLang);
+            }
+            
+            if (GUILayout.Button("Save Changes"))
+            {
+                currentPath = path;
+                SaveChanges();
+            }
+            if (GUILayout.Button("Reload File"))
+            {
+                reloaded = true;
+                Repaint();
+                return;
+            }
+            
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            
+            /*
+             * Language Specific
+             */
+            languageFoldout = EditorGUILayout.Foldout(languageFoldout, new GUIContent("Languages"));
+            if (languageFoldout)
+            {
+                foreach (var language in supportedLanguages)
+                {
+                    EditorGUILayout.BeginHorizontal("HelpBox");
+                    GUILayout.Label(language.ToString());
+                    if (GUILayout.Button("Remove", EditorStyles.miniButton))
+                    {
+                        RemoveLanguage(language);
+                    }
+
+                    EditorGUILayout.EndHorizontal();
+                }
+
+                EditorGUILayout.BeginHorizontal("HelpBox");
+                newLanguage = EditorGUILayout.TextField("New Language", newLanguage);
+                if (GUILayout.Button("Add", EditorStyles.miniButton))
+                {
+                    AddLanguage(supportedLanguages);
+                }
+
+                EditorGUILayout.EndHorizontal();
+            }
+
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            /*
+             * Content Specific
+             */
+            scrollPositionContent = EditorGUILayout.BeginScrollView(scrollPositionContent);
+            EditorGUILayout.BeginVertical();
+            foreach (var t in translations)
+            {               
+                EditorGUILayout.BeginVertical("HelpBox");
+                var languageKeyword = t.Key;
+                var translation = t.Value;
+
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.TextField(languageKeyword, EditorStyles.largeLabel);
+
+                if (GUILayout.Button("Remove", style))
+                {
+                    translations.Remove(languageKeyword);
+                    Repaint();
+                }
+                EditorGUILayout.EndHorizontal();
+                
+                foreach (var lang in supportedLanguages)
+                {  
+                    if(!translation.Values.ContainsKey(lang))
+                        translation.Values.Add(lang, "");
+                    translation.Values[lang] = EditorGUILayout.TextField(lang.ToString(), translation.Values[lang]);
+                }
+                EditorGUILayout.EndVertical();
+            }
+            
+            if(addingTranslation == null)
+            {
+                if (GUILayout.Button("Add Element"))
+                {
+                    addingTranslation = new Translation("");
+                    Repaint();
+                }
+            }
+            else
+            {
+                EditorGUILayout.BeginVertical("HelpBox");
+                addingTranslation.Key = EditorGUILayout.TextField("Key", addingTranslation.Key);
+                foreach (var lang in supportedLanguages)
+                {  
+                    if(!addingTranslation.Values.ContainsKey(lang))
+                        addingTranslation.Values.Add(lang, "");
+                    addingTranslation.Values[lang] = EditorGUILayout.TextField(lang.ToString(), addingTranslation.Values[lang]);
+                }
+
+                if (GUILayout.Button("Add"))
+                {
+                    if(translations.ContainsKey(addingTranslation.Key))
+                        ShowNotification(new GUIContent("Key already used"));
+                    else if (addingTranslation.Key == "")
+                        ShowNotification(new GUIContent("Empty key"));
+                    else 
+                    {
+                        translations.Add(addingTranslation.Key, addingTranslation);
+                        addingTranslation = null;
+                        Repaint();
+                    }
+                }
+                if (GUILayout.Button("Remove"))
+                {
+                    addingTranslation = null;
+                    Repaint();
+                }
+                
+                EditorGUILayout.EndVertical();
+            }
+            
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.EndScrollView();
+        }
+
+        private void OnSelectionChange()
+        {
+            reloaded = true;
+            Repaint();
+        }
+
+        private void RemoveLanguage(SystemLanguage language)
+        {
+            if(addedLanguages.Contains(language)) addedLanguages.Remove(language);
+            removedLanguages.Add(language);
+            Repaint();
+        }
+        
+        private void AddLanguage(ICollection<SystemLanguage> supportedLanguages)
+        {
+            if (SystemLanguage.TryParse<SystemLanguage>(newLanguage, true, out var parsedLanguage))
+            {
+                addedLanguages.Add(parsedLanguage);
+                if (removedLanguages.Contains(parsedLanguage))
+                    removedLanguages.Remove(parsedLanguage);
+                    
+                if(supportedLanguages.Contains(parsedLanguage))
+                    ShowNotification(new GUIContent("Language is already added!"));
+                Repaint();
+            }
+            else
+            {
+                ShowNotification(new GUIContent("Cannot add unsupported Language!"));
+            }
+        }
+
+        private void SaveChanges()
+        {
+            if (!LanguageManager.SaveMlgFile(currentPath, translations, supportedLanguages.ToList(), out var lastError))
+            {
+                ShowNotification(new GUIContent("Could not save file:\n" + lastError));
+            }
+            else
+                ShowNotification(new GUIContent("Saved\n" + currentPath));
+
+            reloaded = true;
+            addingTranslation = null;
+            AssetDatabase.Refresh();
+            UndoChanges();
+        }
+
+        private void UndoChanges()
+        {
+            newLanguage = "";
+            addedLanguages.Clear();
+            removedLanguages.Clear();
+            addingTranslation = null;
+            Repaint();
+        }
+    }
+}
--- Assets/Plugins/LanguageManager/Scripts/Runtime/LanguageManager.cs
+++ Assets/Plugins/LanguageManager/Scripts/Runtime/LanguageManager.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Linq;
@@ -22,6 +24,9 @@ namespace GEAR.Localization
         [SerializeField]
         [SerializeProperty("CurrentLanguage")]
         private SystemLanguage _currentLanguage = SystemLanguage.English;
+
+        private string _lastError = "";
+        public string LastError => _lastError;
 
         public SystemLanguage CurrentLanguage
         {
@@ -88,10 +93,13 @@ namespace GEAR.Localization
 
         public bool LoadMlgFile(TextAsset mlgFile)
         {
+            // return LoadMlgFile(mlgFile, _xmlSchemaSet, _translations, out _lastError, true);
+            
             var result = true;
             if (!mlgFile)
             {
-                Debug.LogError($"LanguageManager::LoadMlgFile: Unable to load language file");
+                _lastError = $"LanguageManager::LoadMlgFile: Unable to load language file";
+                Debug.LogError(_lastError);
                 return false;
             }
 
@@ -115,7 +123,8 @@ namespace GEAR.Localization
                     {
                         if (!Enum.TryParse(languageElement.Name.ToString(), out SystemLanguage language))
                         {
-                            Debug.Log($"LanguageManager::LoadMlgFile: Unsupported Language {languageElement.Name}");
+                            _lastError = $"LanguageManager::LoadMlgFile: Unsupported Language {languageElement.Name}";
+                            Debug.Log(_lastError);
                             result = false;
                             continue;
                         }
@@ -129,10 +138,133 @@ namespace GEAR.Localization
             catch (Exception e)
             {
                 Debug.Log(e.Message);
+                _lastError = e.Message;
                 return false;
             }
 
             return result;
+        }
+        
+        public static bool LoadMlgFile(TextAsset mlgFile, XmlSchemaSet xmlSchemaSet, Dictionary<string, Translation> translations, out string lastError, out HashSet<SystemLanguage> supportedLanguages, bool outputDebug)
+        {
+            lastError = "";
+            supportedLanguages = new HashSet<SystemLanguage>();
+            var result = true;
+            if (!mlgFile)
+            {
+                lastError = $"LanguageManager::LoadMlgFile: Unable to load language file";
+                if(outputDebug)
+                    Debug.LogError(lastError);
+                return false;
+            }
+            
+            try
+            {
+                var doc = XDocument.Load(new MemoryStream(mlgFile.bytes));
+                var tmpError = "";
+                doc.Validate(xmlSchemaSet, (o, e) =>
+                {
+                    tmpError = e.Message;
+                    if(outputDebug)
+                        Debug.Log(e.Message);
+                    result = false;
+                });
+                lastError = tmpError;
+
+                foreach (var translationElement in doc.Descendants("Translation"))
+                {
+                    var key = translationElement.Attribute("Key")?.Value;
+                    if (key == null)
+                        continue;
+
+                    var translation = new Translation(key);
+                    foreach (var languageElement in translationElement.Elements())
+                    {
+                        if (!Enum.TryParse(languageElement.Name.ToString(), out SystemLanguage language))
+                        {
+                            lastError = $"LanguageManager::LoadMlgFile: Unsupported Language {languageElement.Name}";
+                            if(outputDebug)
+                                Debug.Log(lastError);
+                            result = false;
+                            continue;
+                        }
+
+                        supportedLanguages.Add(language);
+                        translation.AddTranslation(language, Regex.Unescape(languageElement.Value));
+                    }
+
+                    translations[key] = translation;
+                }
+            }
+            catch (Exception e)
+            {
+                if(outputDebug)
+                    Debug.Log(e.Message);
+                lastError = e.Message;
+                return false;
+            }
+
+            return result;
+        }
+
+        public static bool SaveMlgFile(string path, Dictionary<string, Translation> translations, List<SystemLanguage> writingLanguages,
+            out string lastError)
+        {
+            lastError = "";
+            try
+            {
+                if (!path.EndsWith(".xml"))
+                {
+                    lastError = "No xml file.";
+                    return false;
+                }
+
+                using (var file = new StreamWriter(path, false))
+                {
+                    file.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+                    file.WriteLine("<LanguageManager>");
+                    file.WriteLine("  <Translations>");
+
+                    foreach (var translation in translations)
+                    {
+                        file.WriteLine("    <Translation Key=\"" + translation.Key + "\">");
+
+                        foreach (var value in translation.Value.Values)
+                        {
+                            if (writingLanguages.Any() && !writingLanguages.Contains(value.Key)) 
+                                continue;
+                            
+                            var val = new StringBuilder();
+                            if (value.Value.Any(c => c >= 255 || c == '&'))
+                            {
+                                foreach (var c in value.Value)
+                                {
+                                    if (c >= 255 || c == '&')
+                                        val.Append($@"\u{(ushort) c:x4}");
+                                    else
+                                        val.Append(c);
+                                }
+                            }
+                            else
+                                val.Append(value.Value);
+
+                            file.WriteLine("      <" + value.Key + ">" + val + "</" + value.Key + ">");
+                        }
+
+                        file.WriteLine("    </Translation>");
+                    }
+
+                    file.WriteLine("  </Translations>");
+                    file.WriteLine("</LanguageManager>");
+                }
+                return true;
+            } 
+            catch (Exception e)
+            {
+                lastError = e.Message;
+                Console.WriteLine(e);
+                return false;
+            }
         }
     }
 }
