2e57ee00175be993517da81bedfb249a8fe50694
Sandra <brettschuh@student.tugraz.at>
2020-07-13 10:16:17 +0200

    basic editor

---
+++ Assets/Plugins/LanguageManager/Scripts/Editor/LocalizedTextEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace GEAR.Localization
+{
+    [CustomEditor(typeof(LocalizedText))]
+    public class LocalizedTextEditor : UnityEditor.Editor
+    {
+        private const string TexturePath = "images/logoLanguageManager";
+        private Texture2D logo = null;
+        
+        SerializedProperty keyProperty;
+        SerializedProperty suffixProperty;
+
+        public void OnEnable()
+        {
+            logo = Resources.Load(TexturePath, typeof(Texture2D)) as Texture2D;
+            keyProperty = serializedObject.FindProperty("key");
+            suffixProperty = serializedObject.FindProperty("suffix");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            GUILayout.Label(logo, GUILayout.Height(50), GUILayout.MinHeight(50), GUILayout.ExpandHeight(false));
+            
+            EditorGUILayout.PropertyField(keyProperty);
+            EditorGUILayout.PropertyField(suffixProperty);
+        }
+    }
+}
