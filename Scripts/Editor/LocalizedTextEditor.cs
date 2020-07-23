b94312a0d00d0a1a62d4a8e6a8aadd5bef1050db
sandra-bre <brettschuh@student.tugraz.at>
2020-07-23 13:59:47 +0200

    Utilities Added New Logos (#28)

--- Scripts/Editor/LocalizedTextEditor.cs
+++ Scripts/Editor/LocalizedTextEditor.cs
@@ -8,9 +8,9 @@ namespace GEAR.Localization
     [CustomEditor(typeof(LocalizedTextBase), true)]
     public class LocalizedTextEditor : UnityEditor.Editor
     {
-        private const string TexturePathDarkTheme = "images/logo_darkTheme";
-        private const string TexturePathLightTheme = "images/logo_lightTheme";
-        private const float logoHeight = 40;
+        private const string TexturePathDarkTheme = "images/LM_logo_darkTheme";
+        private const string TexturePathLightTheme = "images/LM_logo_lightTheme";
+        private const float logoHeight = 48;
         private Texture2D logo = null;
         
         SerializedProperty keyProperty;
