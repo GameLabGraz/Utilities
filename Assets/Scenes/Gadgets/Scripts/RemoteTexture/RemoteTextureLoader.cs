af873b9387d327b3f8d8c6eced4b1423ddf12547
Michael Stefan Holly <michael.holly@tugraz.at>
2020-11-06 15:02:58 +0100

    Utilities-37 [Gadgets] Remote Texture

---
+++ Assets/Scenes/Gadgets/Scripts/RemoteTexture/RemoteTextureLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using GEAR.Gadgets.RemoteTexture;
+
+[RequireComponent(typeof(Image))]
+public class RemoteTextureLoader : MonoBehaviour
+{
+    [SerializeField] private string url;
+
+    private void Start()
+    {
+        var image = GetComponent<Image>();
+        _ = LoadImage(image);
+    }
+
+    private async Task LoadImage(Image image)
+    {
+        image.sprite = await RemoteTexture.GetSprite(url);
+    }
+}
