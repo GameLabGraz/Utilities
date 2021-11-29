69d9128eedc27b192a71784c9854c258009dd289
Michael Stefan Holly <michael.holly@tugraz.at>
2021-11-29 14:50:56 +0100

    Utilities-42 [User Interface] Package (#43)

---
+++ Scripts/Runtime/InputField.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+
+namespace GameLabGraz.UI
+{
+    public class InputField : TMP_InputField
+    {
+        public void SetText(float value)
+        {
+            text = $"{value:0.###}";
+        }
+    }
+}
---
+++ Scripts/Runtime/UserInterfaceContent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace GameLabGraz.UI
+{
+    public static class UserInterfaceContent
+    {
+        public static Object TextPrefab => Resources.Load("Text");
+        public static Object InputPrefab => Resources.Load("InputField");
+        public static Object SliderPrefab => Resources.Load("SliderGroup");
+        public static Object TogglePrefab => Resources.Load("ToggleGroup");
+    }
+}
