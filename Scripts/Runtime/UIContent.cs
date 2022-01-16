3c4f8ec5eb11b4121a8108f8c801aeb78ea4ded8
Michael Stefan Holly <michael.holly@tugraz.at>
2022-01-16 18:35:08 +0100

    Utilities-44 LimeSurveyUploader Remote Control API (#46)

---
+++ Scripts/Runtime/UIContent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace GameLabGraz.UI
+{
+    public static class UIContent
+    {
+        // Layout Groups
+        public static Object VerticalLayoutGroup => Resources.Load("LayoutGroup/VerticalLayoutGroup");
+        public static Object HorizontalLayoutGroup => Resources.Load("LayoutGroup/HorizontalLayoutGroup");
+
+        // UI Elements
+        public static Object Text => Resources.Load("Text");
+        public static Object Input => Resources.Load("InputField");
+        public static Object SliderPrefab => Resources.Load("SliderGroup");
+        public static Object ToggleButton => Resources.Load("ToggleButton");
+        public static Object ToggleGroup => Resources.Load("ToggleGroup");
+        public static Object RadioButton => Resources.Load("RadioButton");
+        public static Object RadioButtonGroup => Resources.Load("RadioButtonGroup");
+        public static Object Button => Resources.Load("Button");
+    }
+}
