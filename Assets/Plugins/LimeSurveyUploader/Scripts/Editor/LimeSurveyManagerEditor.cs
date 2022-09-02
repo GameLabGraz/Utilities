a692bc9f7e575c56ebc576ee6dd38e1b3900fc7e
Sebastian <sebastian.resch@student.tugraz.at>
2022-09-02 13:44:12 +0200

    Utilities [LimesurveyUploader] Config File (#60)

--- Assets/Plugins/LimeSurveyUploader/Scripts/Editor/LimeSurveyManagerEditor.cs
+++ Assets/Plugins/LimeSurveyUploader/Scripts/Editor/LimeSurveyManagerEditor.cs
@@ -10,19 +10,11 @@ namespace GameLabGraz.LimeSurvey.Editor
         private const string TexturePathLightTheme = "images/LIME_logo_lightTheme";
         private const float logoHeight = 48;
         private Texture2D _logoTexture;
-
-        private SerializedProperty url;
-        private SerializedProperty userName;
-        private SerializedProperty password;
         private SerializedProperty surveyId;
 
         private void OnEnable()
         {
             _logoTexture = Resources.Load<Texture2D>(EditorGUIUtility.isProSkin ? TexturePathDarkTheme : TexturePathLightTheme);
-
-            url = serializedObject.FindProperty("url");
-            userName = serializedObject.FindProperty("userName");
-            password = serializedObject.FindProperty("password");
             surveyId = serializedObject.FindProperty("surveyId");
         }
 
@@ -32,10 +24,15 @@ namespace GameLabGraz.LimeSurvey.Editor
 
             serializedObject.Update();
 
-            EditorGUILayout.PropertyField(url);
-            EditorGUILayout.PropertyField(userName);
-            password.stringValue = EditorGUILayout.PasswordField(password.displayName, password.stringValue);
+            // Server Configuration
+            EditorGUILayout.LabelField("Server Config", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("Please enter the server configuration in the \"LimeSurveyServerConfig.json\" file");
+            EditorGUILayout.Space();
+
+            // Survey Configuration
+            EditorGUILayout.LabelField("Survey Config", EditorStyles.boldLabel);
             EditorGUILayout.PropertyField(surveyId);
+            EditorGUILayout.Space();
             
             serializedObject.ApplyModifiedProperties();
         }
