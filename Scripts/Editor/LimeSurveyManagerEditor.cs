a883de54f3f3835ae1086eccbe3d6497e1d1df8f
Sebastian <sebastian.resch@student.tugraz.at>
2022-09-08 12:06:09 +0200

    Generate config files from UI (#64)

--- Scripts/Editor/LimeSurveyManagerEditor.cs
+++ Scripts/Editor/LimeSurveyManagerEditor.cs
@@ -6,11 +6,27 @@ namespace GameLabGraz.LimeSurvey.Editor
     [CustomEditor(typeof(LimeSurveyManager))]
     public class LimeSurveyManagerEditor : UnityEditor.Editor
     {
+        // ------------
+        // Members
+
+        // Image
         private const string TexturePathDarkTheme = "images/LIME_logo_darkTheme";
         private const string TexturePathLightTheme = "images/LIME_logo_lightTheme";
         private const float logoHeight = 48;
         private Texture2D _logoTexture;
+
+        // Server Config
+        private bool configActive = false;
+        private bool configSaved = false;
+        private string url = "url/index.php/admin/remotecontrol";
+        private string username = "username";
+        private string password = "password";
+
+        // Survey
         private SerializedProperty surveyId;
+
+        // ------------
+        // Methods
 
         private void OnEnable()
         {
@@ -21,12 +37,46 @@ namespace GameLabGraz.LimeSurvey.Editor
         public override void OnInspectorGUI()
         {
             DrawLogoTexture();
-
             serializedObject.Update();
 
             // Server Configuration
-            EditorGUILayout.LabelField("Server Config", EditorStyles.boldLabel);
-            EditorGUILayout.LabelField("Please enter the server configuration in the \"LimeSurveyServerConfig.json\" file");
+            EditorGUILayout.LabelField("Server Configuration", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("Configuration is stored here:");
+            EditorGUILayout.LabelField("LimeSurveyServerConfig.json");
+            EditorGUILayout.Space();
+
+            if (!configActive)
+            {
+                if(GUILayout.Button("Generate Config File"))
+                {
+                    configSaved = false;
+                    configActive = true;
+                }
+
+                if(configSaved)
+                {
+                    EditorGUILayout.LabelField("Saved config file!");
+                }
+            }
+
+            if(configActive)
+            {
+                EditorGUILayout.LabelField("URL");
+                url = EditorGUILayout.TextField(url);
+
+                EditorGUILayout.LabelField("Username");
+                username = EditorGUILayout.TextField(username);
+
+                EditorGUILayout.LabelField("Password");
+                password = EditorGUILayout.PasswordField(password);
+
+                if(GUILayout.Button("Save Config File"))
+                {
+                    SaveConfigFile();
+                }
+            }
+
+            EditorGUILayout.Space();
             EditorGUILayout.Space();
 
             // Survey Configuration
@@ -44,5 +94,43 @@ namespace GameLabGraz.LimeSurvey.Editor
                 GUILayout.Label(_logoTexture, GUILayout.Height(logoHeight), GUILayout.MinHeight(logoHeight), GUILayout.MaxHeight(logoHeight), GUILayout.ExpandHeight(false));
             }
         }
+
+        private void SaveConfigFile()
+        {
+            // Create directory
+            string dirPath = Application.dataPath + "/Resources/LimeSurveyServerConfig/";
+            System.IO.Directory.CreateDirectory(dirPath);
+
+            // Write .gitignore
+            string gitPath = dirPath + ".gitignore";
+            string[] gitLines =
+            {
+                "/LimeSurveyServerConfig.json",
+                "/LimeSurveyServerConfig.json.meta"
+            };
+
+            System.IO.File.WriteAllLines(gitPath, gitLines);
+
+            // Write Config file
+            string configPath = dirPath + "LimeSurveyServerConfig.json";
+            string[] configLines =
+            {
+                "{",
+                "    \"url\": \"" + url + "\",",
+                "    \"username\": \"" + username + "\",",
+                "    \"password\": \"" + password + "\"",
+                "}"
+            };
+
+            System.IO.File.WriteAllLines(configPath, configLines);
+
+            // Trigger asset refresh so that Unity finds newly generated files
+            AssetDatabase.Refresh();
+
+            // Update state
+            configActive = false;
+            configSaved = true;
+        }
+        
     }
 }
